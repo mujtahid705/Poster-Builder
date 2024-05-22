d9286c0ca4fd539c191047fe9ab6c17ef43b05e1
Muhammad Mujtahid <mujtahid705@gmail.com>
2024-05-22 18:26:17 +0600

    more editor features added

--- src/components/DragFG.tsx
+++ src/components/DragFG.tsx
@@ -1,21 +1,38 @@
-import Draggable from "react-draggable";
+import Draggable, {
+  DraggableEvent,
+  DraggableEventHandler,
+} from "react-draggable";
 import styles from "./DragFG.module.css";
 
 interface DraggableItemProps {
   defaultPosition?: { x: number; y: number };
   bounds?: string;
   image: string;
+  width?: number;
+  zIndex?: number;
+  onStop?: DraggableEventHandler;
 }
 
 const DragFG: React.FC<DraggableItemProps> = ({
   defaultPosition,
   bounds,
   image,
+  width,
+  zIndex,
+  onStop,
 }) => {
   return (
     <>
-      <Draggable defaultPosition={defaultPosition} bounds={bounds}>
-        <img src={image} className={styles.dragImg} />
+      <Draggable
+        defaultPosition={defaultPosition}
+        bounds={bounds}
+        onStop={onStop}
+      >
+        <img
+          src={image}
+          className={styles.dragImg}
+          style={{ width: `${width}px`, zIndex: zIndex }}
+        />
       </Draggable>
     </>
   );
--- src/pages/EditorPage.tsx
+++ src/pages/EditorPage.tsx
@@ -1,32 +1,32 @@
 import styles from "./EditorPage.module.css";
 // import background from "../assets/poster_bg.png";
-import fg1 from "../assets/fg1.png";
-import fg2 from "../assets/fg2.png";
+// import fg1 from "../assets/fg1.png";
+// import fg2 from "../assets/fg2.png";
 import DragFG from "../components/DragFG";
-import { useRef, useState } from "react";
-
-// const __Template = {
-//     title: "",
-//     description: "",
-//     background: "",
-//     image: "",
-//     fg_s: ["", ""]
-// }
+import { DraggableData, DraggableEvent } from "react-draggable";
+import { useState } from "react";
 
 const EditorPage: React.FC = () => {
-  const titleRef = useRef<HTMLInputElement>(null);
-  const descriptionRef = useRef<HTMLInputElement>(null);
-  const backgroundRef = useRef<HTMLInputElement>(null);
-
   const [title, setTitle] = useState<string>("");
   const [description, setDescription] = useState<string>("");
   const [background, setBackground] = useState<string>("");
 
-  const [foreground, setForeground] = useState<{ id: number; image: string }[]>(
-    []
-  );
+  const [image, setImage] = useState<string>("");
+  const [imageWidth, setImageWidth] = useState<number>(200);
+  const [imageZIdx, setImageZIdx] = useState<number>(0);
+
+  const [foreground, setForeground] = useState<
+    {
+      id: number;
+      image: string;
+      width: number;
+      position: { x: number; y: number };
+    }[]
+  >([]);
   const [foregroundList, setForegroundList] = useState<number[]>([]);
   const [foregroundCounter, setForgroundCounter] = useState<number>(0);
+
+  console.log(foreground);
 
   const addForegroundHandler = () => {
     setForegroundList((prev) => [...prev, foregroundCounter]);
@@ -43,7 +43,12 @@ const EditorPage: React.FC = () => {
       reader.onloadend = () => {
         setForeground((prev) => [
           ...prev,
-          { id: id, image: reader.result as string },
+          {
+            id: id,
+            image: reader.result as string,
+            width: 100,
+            position: { x: 50, y: 50 },
+          },
         ]);
       };
       reader.readAsDataURL(file);
@@ -55,22 +60,42 @@ const EditorPage: React.FC = () => {
     setForegroundList((prev) => prev.filter((item) => item !== idx));
   };
 
-  const applyHandler = () => {
-    if (titleRef.current) setTitle(titleRef.current?.value);
-    if (descriptionRef.current) setDescription(descriptionRef.current?.value);
-
-    if (backgroundRef.current?.files) {
-      const file = backgroundRef.current.files[0];
-      if (file) {
-        const reader = new FileReader();
-        reader.onload = (e) => {
-          if (e.target?.result) {
-            setBackground(e.target.result as string);
-          }
-        };
-        reader.readAsDataURL(file);
-      }
+  const addimageHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImage(reader.result as string);
+      };
+      reader.readAsDataURL(file);
     }
+  };
+
+  const addBackgroundHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setBackground(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const addTitleHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target?.value);
+  };
+
+  const addDescriptionHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDescription(e.target?.value);
+  };
+
+  const handleStop = (id: number, e: DraggableEvent, data: DraggableData) => {
+    setForeground((prevForeground) =>
+      prevForeground.map((fg) =>
+        fg.id === id ? { ...fg, position: { x: data.x, y: data.y } } : fg
+      )
+    );
   };
 
   //   const title = "TAKE YOUR SKILL TO THE NEXT LEVEL";
@@ -80,38 +105,73 @@ const EditorPage: React.FC = () => {
     <>
       <div className={styles.container}>
         <div className={styles.editorForm}>
-          <input ref={titleRef} type="text" placeholder="Title" />
-          <input ref={descriptionRef} type="text" placeholder="Description" />
-          <input ref={backgroundRef} type="file" accept="img/" />
+          <input type="text" placeholder="Title" onChange={addTitleHandler} />
+          <input
+            type="text"
+            placeholder="Description"
+            onChange={addDescriptionHandler}
+          />
+          <input type="file" accept="img/" onChange={addBackgroundHandler} />
+          <input type="file" accept="img/" onChange={addimageHandler} />
+          <input
+            type="number"
+            placeholder="Image Width"
+            onChange={(e) => setImageWidth(parseInt(e.target.value))}
+          />
+          {image && imageZIdx === 0 && (
+            <button onClick={() => setImageZIdx(1)}>Bring to Front</button>
+          )}
+          {imageZIdx === 1 && (
+            <button onClick={() => setImageZIdx(0)}>Bring to Back</button>
+          )}
 
           <button onClick={addForegroundHandler}>Add FG</button>
           {foregroundList.map((item) => (
             <>
-              <input
-                key={item}
-                type="file"
-                accept="img/"
-                onChange={(e) => foregroundImgHandler(e, item)}
-              />
-              <button onClick={() => deleteForegroundImg(item)}>Delete</button>
+              <div className={styles.foregroundInputContainer}>
+                <input
+                  key={item}
+                  type="file"
+                  accept="img/"
+                  onChange={(e) => foregroundImgHandler(e, item)}
+                />
+                <input
+                  type="number"
+                  placeholder="FG Width"
+                  onChange={(e) => {
+                    setForeground((prev) =>
+                      prev.map((el) =>
+                        el.id === item
+                          ? { ...el, width: parseInt(e.target.value) }
+                          : el
+                      )
+                    );
+                  }}
+                />
+                <button onClick={() => deleteForegroundImg(item)}>
+                  Delete
+                </button>
+              </div>
             </>
           ))}
-
-          <button onClick={applyHandler}>Apply</button>
         </div>
 
         <div className={styles.editorContainer} id="editorContainer">
-          {/* <DragFG
+          <DragFG
             defaultPosition={{ x: 50, y: 50 }}
             bounds="#editorContainer"
-            image={fg1}
-          /> */}
+            image={image}
+            width={imageWidth}
+            zIndex={imageZIdx}
+          />
           {foreground.map((item, i) => (
             <DragFG
               key={i}
-              defaultPosition={{ x: 50, y: 50 }}
+              defaultPosition={item.position}
               bounds="#editorContainer"
               image={item.image}
+              width={item.width}
+              onStop={(e, data) => handleStop(item.id, e, data)}
             />
           ))}
           <img src={background} className={styles.backgroundImg} />
